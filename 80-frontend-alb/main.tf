0330fddea092cf423ec313133692f1035dbd884a
mayuri <mayu.reddy10@gmail.com>
2026-05-05 15:05:59 +1000

    k8

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "frontend_alb" {
     name = "${var.project}-${var.environment}-frontend"  #roboshop-dev-frontend
     internal = false
     load_balancer_type = "application"
-    security_groups = [local.frontend_alb_sg_id]
+    security_groups = [local.ingress_alb_sg_id]
     subnets = local.public_subnet_ids
 
     #keeping it as false, just to delete using terraform while practice
